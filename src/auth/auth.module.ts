4b2ccc0e47f1f1e90708cba46d4c712af25fe907
Michael Gian Tiqui <Michael.Tiqui@WMC.local>
2024-09-23 13:21:06 +0800

    auth should only accept access token and not refresh token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,18 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       secret: process.env.AT_SECRET,
       signOptions: { expiresIn: process.env.AT_EXP },
     }),
   ],
-  providers: [AuthService, PrismaService],
+  providers: [AuthService, PrismaService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
